a0712d94543b1aaba7451f19ce348c1f4a259da2
iure <idknameuri@outlook.com>
2022-06-16 11:13:24 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import Header from './componentes/Header';
+import FormularioTareas from './componentes/FormularioTareas';
+import ListasTareas from './componentes/ListaTareas';
+import { useEffect } from 'react';
 
-function App() {
+const App = () => {
+  //Obtenemos las tyareas guardadas del local storage
+  const tareasGuardadas = 
+  localStorage.getItem('tareas') ? 
+  JSON.parse(localStorage.getItem('tareas')) : [];
+  
+  //Establecemos el estado de las tareas
+  const [tareas, cambiarTareas] = useState(tareasGuardadas);
+  
+  // Guardando el estado de localstorage
+  useEffect(()=> {
+    localStorage.setItem('tareas', JSON.stringify(tareas)); 
+  },[tareas]);
+
+  // Accedemos a localstorage y comprobamos si MostrarCompletadas es null
+  let configMostrarCompletadas = ''; 
+  if(localStorage.getItem('mostrarCompletadas') === null){
+    configMostrarCompletadas = true;
+  } else {
+    configMostrarCompletadas = localStorage.getItem('mostrarCompletadas') === 'true';
+  }
+
+  // El estado de mostrarCompletadas
+  const [mostrarCompletadas, cambiarMostrarCompletadas] = useState(configMostrarCompletadas);
+
+  useEffect(()=> { 
+    localStorage.setItem('mostrarCompletadas', mostrarCompletadas.toString()); 
+    },[mostrarCompletadas]);
+  
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='contenedor'>     
+     <Header 
+        mostrarCompletadas={mostrarCompletadas} 
+        cambiarMostrarCompletadas= {cambiarMostrarCompletadas}
+     />
+     <FormularioTareas tareas={tareas} cambiarTareas={cambiarTareas} />
+     <ListasTareas 
+     tareas={tareas} 
+     cambiarTareas={cambiarTareas}
+     mostrarCompletadas={mostrarCompletadas}
+     />
     </div>
   );
 }
